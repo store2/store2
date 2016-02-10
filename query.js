a38efac3594fb5274cded515030b4c27e084bea8
42 <42@jso>
2016-02-10 00:12:33 +0000

    query

--- query.js
+++ query.js
@@ -15,41 +15,41 @@ define( ['./dom','./ndrscr'],function( dom, _ ) {
 						on: { 'click': function( e, ev ){ el.rm( ); }}} )] }) 
 		])
 		return el; }
-	dom.popup_xhrerr = function( a, d ){
+	dom.popup_xhrerr = function( d ){
 		dom.msg_xhrerr( d, dom.byid( 'msg' )); }
 
-	var jsontype = 'application/json';
+	var jsontype = 'application/json',
+		formtype = 'application/x-www-form-urlencoded';
 	var query = function( target, data, opt ){
 		data = data || {};
 		opt = opt || {};
-		var url = target,
+  	var url = target,
 			xhr = new XMLHttpRequest(),
 			method = opt.method || 'get',
-			accept = jsontype,
-			ctype = jsontype;
+			accept = opt.accept || jsontype,
+			ctype = opt.ctype || jsontype;
 		if( window.location.protocol === 'file:' ){
 			url = 'http://localhost'+url; }
+		//xhr.withCredentials = true;
 		xhr.open( method, url, true);
-		if( typeof data === 'string' ){
-			ctype = 'application/x-www-form-urlencoded' }
 		xhr.setRequestHeader( 'Content-type', ctype );
 		xhr.setRequestHeader( 'Accept', accept );
-		if( opt.auth ){
+		if( opt.auth && opt.auth.name ){
 			var authstr = 'Basic '+btoa( opt.auth.name+':'+opt.auth.password );
 			xhr.setRequestHeader('Authorization', authstr) }
 		xhr.onreadystatechange = function( ){
+			//console.log( {xhr:xhr} );
 			if( _.isFunction( opt['statuscb'] )){
 				opt['statuscb']( d ); }
-			if( xhr.readyState == 4 ){
-				var d = false, r = xhr.responseText || { };
-				if( accept === jsontype ){
+  	  if( xhr.readyState == 4 ){
+				var d = false, r = xhr.responseText || false;
+				if( r && accept === jsontype ){
 					d = JSON.parse( r ) }
 				if( typeof xhr['status'] === 'undefined' ){
 					var info = {status:'502',msg:'bad gateway',data:d};
 					if( _.isFunction( opt['error'] )){ opt['error']( xhr ); }
 					dom.popup_xhrerr( info )}
-				else if( xhr['status'] === 200 ||
-					xhr['status'] === '201' ){
+				else if( [200,201,202].indexOf(xhr['status']) != -1 ){
 					if( _.isFunction( opt['success'] )){ opt['success']( d ); }}
 				else{
 					var info = {'status':xhr['status'],msg:xhr.statusText,data:d};
